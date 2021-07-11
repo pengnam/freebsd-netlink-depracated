869008fbbd8b65ce69bf885520c59645dc076862
Sean Ng <seanngpengnam@gmail.com>
2021-07-11 14:22:00 +0000

    Shift nla stuff into net header

--- net/netlink.h
+++ net/netlink.h
@@ -157,5 +157,84 @@ struct nlpcb {
 #define _M_NLPROTO(m)  ((m)->m_pkthdr.rsstype)  /* netlink proto, 8 bit */
 #define NETISR_NETLINK  15  // XXX hack, must be unused and < 16
 
+ /**
+  * Standard attribute types to specify validation policy
+  */
+enum {
+	NLA_UNSPEC,
+	NLA_U8,
+	NLA_U16,
+	NLA_U32,
+	NLA_U64,
+	NLA_S8,
+	NLA_S16,
+	NLA_S32,
+	NLA_S64,
+	NLA_STRING,
+	NLA_FLAG,
+	NLA_REJECT,
+	NLA_NESTED,
+	NLA_NESTED_ARRAY,
+	NLA_NUL_STRING,
+	__NLA_TYPE_MAX,
+};
+#define NLA_TYPE_MAX (__NLA_TYPE_MAX - 1)
+struct nla_policy {
+    uint16_t        type;
+    uint16_t        len;
+    struct nla_policy *nested_policy;
+
+};
+
+static const uint8_t nla_attr_len[NLA_TYPE_MAX+1] = {
+	[NLA_U8]	= sizeof(uint8_t),
+	[NLA_U16]	= sizeof(uint16_t),
+	[NLA_U32]	= sizeof(uint32_t),
+	[NLA_U64]	= sizeof(uint64_t),
+	[NLA_S8]	= sizeof(int8_t),
+	[NLA_S16]	= sizeof(int16_t),
+	[NLA_S32]	= sizeof(int32_t),
+	[NLA_S64]	= sizeof(int64_t),
+};
+
+static const uint8_t nla_attr_minlen[NLA_TYPE_MAX+1] = {
+	[NLA_U8]	= sizeof(uint8_t),
+	[NLA_U16]	= sizeof(uint16_t),
+	[NLA_U32]	= sizeof(uint32_t),
+	[NLA_U64]	= sizeof(uint64_t),
+	//[NLA_MSECS]	= sizeof(uint64_t),
+	[NLA_S8]	= sizeof(int8_t),
+	[NLA_S16]	= sizeof(int16_t),
+	[NLA_S32]	= sizeof(int32_t),
+	[NLA_S64]	= sizeof(int64_t),
+};
+#define NLA_ALIGNTO		4
+#define NLA_ALIGN(len)		(((len) + NLA_ALIGNTO - 1) & ~(NLA_ALIGNTO - 1))
+#define NLA_HDRLEN		((int) NLA_ALIGN(sizeof(struct nlattr)))
+
+/**
+ * nla_for_each_attr - iterate over a stream of attributes
+ * @pos: loop counter, set to current attribute
+ * @head: head of attribute stream
+ * @len: length of attribute stream
+ * @rem: initialized to len, holds bytes currently remaining in stream
+ */
+#define nla_for_each_attribute(pos, head, len, rem) \
+	for (pos = head, rem = len; \
+	     nla_ok(pos, rem); \
+	     pos = nla_next(pos, &(rem)))
+
+/**
+ * nla_for_each_nested - iterate over nested attributes
+ * @pos: loop counter, set to current attribute
+ * @nla: attribute containing the nested attributes
+ * @rem: initialized to len, holds bytes currently remaining in stream
+ */
+#define nla_for_each_nested(pos, nla, rem) \
+	nla_for_each_attr(pos, nla_data(nla), nla_len(nla), rem)
+
+
+#define MAX_POLICY_RECURSION_DEPTH 10
+
 
 #endif
