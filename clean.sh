5132763d803a9d39a7d2aec8e35acdb2e8f8f6ba
Sean Ng <seanngpengnam@gmail.com>
2021-07-18 08:18:04 +0000

    Add copy of linux's header files

--- clean.sh
+++ clean.sh
@@ -3,5 +3,7 @@ rm .nlattr.c.swp
 rm .genetlink.c.swp
 rm net/.netlink.h.swp
 rm net/.genetlink.h.swp
+rm net/.rtnetlink.h.swp
 rm linux/.netlink.h.swp
 rm linux/.genetlink.h.swp
+rm linux/.rtnetlink.h.swp
---
+++ rtnetlink.c
@@ -0,0 +1,5 @@
+#include <net/rtnetlink.h>
+
+
+
+
